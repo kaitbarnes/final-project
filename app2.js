da7b8a4f36d828694b6ec8265f80eacf916b39b6
kaitbarnes <73683336+kaitbarnes@users.noreply.github.com>
2021-06-04 16:42:35 -0400

    navbar

--- app2.js
+++ app2.js
@@ -1,11 +1,25 @@
-/// NAV BAR ///
+//nav bar//
 
-const navbar = document.querySelector(".navbar");
-const menuToggle = document.querySelector(".menu-toggle");
-menuToggle.addEventListener("click", () => {
-  navbar.classList.toggle("open");
-});
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
+    
   ///SLIDESHOW///
 
   var slideIndex = 1;
